a18a754258694f00b9979d3aaef7fe9040b4ab29
Fxzx mic <54622331+fxzxmic@users.noreply.github.com>
2020-12-30 01:10:17 +0800

    Update main.c
    Replace "_splitpath"

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 
 int main(int argc, char *argv[]) {
   //Get program path information
-  char path_buffer[_MAX_PATH];
   char drive[_MAX_DRIVE];
   char dir[_MAX_DIR];
   char fname[_MAX_FNAME];
   char ext[_MAX_EXT];
-  _splitpath(argv[0], drive, dir, fname, ext);
+  _splitpath_s(argv[0], drive, dir, fname, ext);
   //Splice command
   char ch[100] = "bash.exe -c \"";
   strcat_s(ch, sizeof(ch), fname);
