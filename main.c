ad29de822393d1b7f491f3ddf4ce60f188b85f80
Fxzx mic <54622331+fxzxmic@users.noreply.github.com>
2020-12-29 19:46:14 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char path_buffer[_MAX_PATH]; 
+  char drive[_MAX_DRIVE]; 
+  char dir[_MAX_DIR]; 
+  char fname[_MAX_FNAME]; 
+  char ext[_MAX_EXT];
+  _splitpath(argv[0], drive, dir, fname, ext); 
+  char ch[100] = "bash.exe -c \"";
+  strcat_s(ch, sizeof(ch), fname);
+  int count;
+  for (count = 1; count < argc; ++count) {
+    char ch0[100] = " ";
+    strcat_s(ch0, sizeof(ch0), argv[count]);
+    strcat_s(ch, sizeof(ch), ch0);
+  }
+  strcat_s(ch, sizeof(ch), "\"");
+  system(ch);
+
+  return 0;
+}
